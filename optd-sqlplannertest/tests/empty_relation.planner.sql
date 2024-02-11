bff42488f10602b9c80b1f9a4e6421c03a4cb970
AveryQi115 <averyqi115@gmail.com>
2024-02-11 00:06:15 -0500

    fix empty relation test
    Signed-off-by: AveryQi115 <averyqi115@gmail.com>

--- optd-sqlplannertest/tests/empty_relation.planner.sql
+++ optd-sqlplannertest/tests/empty_relation.planner.sql
@@ -36,40 +36,10 @@ select 64+1 from t1 inner join t2 on false;
 
 /*
 LogicalProjection { exprs: [ #0, #1, #2, #3 ] }
-└── LogicalJoin { join_type: Inner, cond: true }
+└── LogicalJoin { join_type: Inner, cond: false }
     ├── LogicalScan { table: t1 }
     └── LogicalScan { table: t2 }
 PhysicalProjection { exprs: [ #0, #1, #2, #3 ] }
-└── PhysicalProjection { exprs: [ #2, #3, #0, #1 ] }
-    └── PhysicalNestedLoopJoin { join_type: Inner, cond: true }
-        ├── PhysicalScan { table: t2 }
-        └── PhysicalScan { table: t1 }
-0 0 0 200
-0 0 1 201
-0 0 2 202
-1 1 0 200
-1 1 1 201
-1 1 2 202
-2 2 0 200
-2 2 1 201
-2 2 2 202
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
-65
+└── PhysicalEmptyRelation { produce_one_row: false }
 */
 

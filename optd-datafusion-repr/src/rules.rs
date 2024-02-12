cfa595b2376316f06fe40e48f89bf2493b961512
Benjamin Owad <jeep70cp@gmail.com>
2024-02-12 17:35:44 -0500

    feat: Eliminate Filter Rule (#54)

--- optd-datafusion-repr/src/rules.rs
+++ optd-datafusion-repr/src/rules.rs
@@ -1,9 +1,11 @@
 // mod filter_join;
+mod eliminate_filter;
 mod joins;
 mod macros;
 mod physical;
 
 // pub use filter_join::FilterJoinPullUpRule;
+pub use eliminate_filter::EliminateFilterRule;
 pub use joins::{
     EliminateJoinRule, HashJoinRule, JoinAssocRule, JoinCommuteRule, ProjectionPullUpJoin,
 };

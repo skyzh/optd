0e2f9641a7902a9a660d59a48c135efc45bf4a7d
Alex Chi <iskyzh@gmail.com>
2024-11-14 22:52:25 -0500

    dump memo
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- optd-sqlplannertest/tests/sorts/sort_passthrough.planner.sql
+++ optd-sqlplannertest/tests/sorts/sort_passthrough.planner.sql
@@ -24,6 +24,44 @@ PhysicalSort
 ├── exprs:SortOrder { order: Asc }
 │   └── #0
 └── PhysicalScan { table: t1 }
+group_id=!2
+  subgroup_id=.15 winner=17 weighted_cost=1000 | (PhysicalScan P0)
+    cost={compute=0,io=1000}
+    stat={row_cnt=1000}
+    sort=<any>
+  subgroup_id=.18 winner=enforcer weighted_cost=7908.754779315221 | enforcer
+    cost={compute=6908.754779315221,io=1000}
+    stat={row_cnt=1000}
+    sort=[Asc#0]
+  schema=[t1v1:Int32, t1v2:Int32]
+  column_ref=[t1.0, t1.1]
+  expr_id=1 | (Scan P0)
+  expr_id=17 | (PhysicalScan P0)
+  P0=(Constant(Utf8String) "t1")
+group_id=!5
+  subgroup_id=.12 winner=14 weighted_cost=7908.754779315221 | (PhysicalSort !2 P3)
+    cost={compute=6908.754779315221,io=1000}
+    stat={row_cnt=1000}
+    sort=<any>
+  subgroup_id=.19 winner=14 weighted_cost=7908.754779315221 | (PhysicalSort !2 P3)
+    cost={compute=6908.754779315221,io=1000}
+    stat={row_cnt=1000}
+    sort=[Asc#0]
+  schema=[t1v1:Int32, t1v2:Int32]
+  column_ref=[t1.0, t1.1]
+  expr_id=4 | (Sort !2 P3)
+  expr_id=14 | (PhysicalSort !2 P3)
+  P3=(List (SortOrder(Asc) (ColumnRef 0(u64))))
+group_id=!8
+  subgroup_id=.9 winner=!5 weighted_cost=7908.754779315221 | !5
+    cost={compute=6908.754779315221,io=1000}
+    stat={row_cnt=1000}
+    sort=<any>
+  schema=[t1v1:Int32, t1v2:Int32]
+  column_ref=[t1.0, t1.1]
+  expr_id=7 | (Sort !5 P3)
+  expr_id=11 | (PhysicalSort !5 P3)
+  P3=(List (SortOrder(Asc) (ColumnRef 0(u64))))
 */
 
 -- test order by passthrough
